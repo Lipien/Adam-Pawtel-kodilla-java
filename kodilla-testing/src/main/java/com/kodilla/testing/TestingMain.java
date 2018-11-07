5793d704d10faae6b4792569f1781d45b93a2d40
Lipien <adam@pawtel.pl>
2018-11-07 09:56:53 +0100

    6.2 first unit test: calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        Integer resultAdd = calculator.add(4, 5);
+        if (resultAdd.equals(9)) {
+            System.out.println("test2 OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Integer resultSubstract = calculator.substract(22, 2);
+
+        if (resultSubstract.equals(20)) {
+            System.out.println("test3 OK");
+        } else {
+            System.out.println("Error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int d, int c) {
+        return d - c;
+    }
+
+}
