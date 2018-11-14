19f8212b54f4f1bdaa4a617588ccf8121bf735a7
Lipien <adam@pawtel.pl>
2018-11-14 21:59:39 +0100

    7.1 Zadanie: Upiększacz tekstów.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println(" ");
+        System.out.println("Zadanie: Upiększacz tekstów:");
+        System.out.println(" ");
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Text Text Text", (String::toUpperCase));
+        poemBeautifier.beautify("Text Text Text", text -> text.toLowerCase());
+        poemBeautifier.beautify("Text Text text", (text -> "***" + text + "***"));
+        poemBeautifier.beautify("TEXT IN CAGES", (text -> {
+            StringBuilder textStringBuit = new StringBuilder();
+            textStringBuit.append("|");
+            for (int i = 0; i < text.length(); i++) {
+                textStringBuit.append(text.charAt(i));
+                textStringBuit.append("|");
+            }
+            return textStringBuit.toString();
+
+        }));
+    }
+}
