652d94ad0f6d2c661a76c02c9727228d5fe74cff
Lipien <adam@pawtel.pl>
2018-11-15 10:09:12 +0100

    Zadanie: niemutowalny użytkownik forum.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 
 public class StreamMain {
@@ -30,5 +31,7 @@ public class StreamMain {
             return textStringBuilt.toString();
 
         }));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
