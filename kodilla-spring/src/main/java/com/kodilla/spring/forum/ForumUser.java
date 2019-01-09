42d65d9f5c76af4e62fda7c48ab94121208165ed
Lipien <adam@pawtel.pl>
2019-01-09 16:35:58 +0100

    14.1 Bean ForumUser.

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    private String username = "John Smith";
+
+    public ForumUser() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
