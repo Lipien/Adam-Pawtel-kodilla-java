d772cb1d29654c207efcb3b5c54069faa2e76418
Lipien <adam@pawtel.pl>
2019-02-04 14:45:03 +0100

    17.3 Tworzymy relacje pomiędzy encjami.

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,7 +1,11 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -9,11 +13,12 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List <Task> tasks = new ArrayList <>();
 
     public TaskList() {
     }
 
-    public TaskList(String listName) {
+    public TaskList(String listName, String description) {
         this.id = id;
         this.listName = listName;
         this.description = description;
@@ -37,6 +42,16 @@ public class TaskList {
         return description;
     }
 
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List <Task> getTasks() {
+        return tasks;
+    }
+
     private TaskList setId(int id) {
         this.id = id;
         return this;
@@ -50,5 +65,9 @@ public class TaskList {
     private TaskList setDescription(String description) {
         this.description = description;
         return this;
+    }
+
+    public void setTasks(List <Task> tasks) {
+        this.tasks = tasks;
     }
 }
